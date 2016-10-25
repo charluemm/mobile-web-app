3521d3e4b5a0161a1c7ef318551250e0b4e64f4b
Michael Müller <mishbag@reu-network.de>
2016-10-25 18:24:36 +0200

    render form on fab click

---
+++ resources/js/app.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    $('#btn-fab-add-task').on('click', function(){
+        $('div[data-tab="tasks"]').load("fragments/form.task.add.html");
+
+        $(document).trigger("createinclude");
+    });
+});
+
+function renderFragment(fragment)
+{
+    var el = this.element;
+    var opts = $.extend(this.options, el.data("options"));
+    $(document).trigger("createinclude");
+
+    if (opts.src !== null) {
+        el.load(opts.src, function() {
+            el.enhanceWithin();
+
+            // Apply waves.js
+            if (typeof Waves !== "undefined") {
+                Waves.attach('a', ['waves-button']);
+                Waves.attach('button', ['waves-button']);
+                Waves.init();
+
+                $("body").find(".ui-flipswitch-on").removeClass("waves-effect");
+                Waves.attach('.ui-flipswitch', ['waves-button', 'waves-light']);
+
+            }
+
+        });
+    }
+}
