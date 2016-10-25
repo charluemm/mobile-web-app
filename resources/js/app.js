c9cea42363947ffbb3cee13bd592b8396eb0aee8
Michael Müller <mishbag@reu-network.de>
2016-10-25 18:48:27 +0200

    bugfix: touch

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,9 @@
 
-$(document).ready(function(){
-    $('#btn-fab-add-task').on('click touchstart', function(e){
-        e.preventDefault();
-        console.log("clicked");
-        $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
-        $('#page1').trigger('pagecreate');
-       // $.mobile.loadPage("fragments/form.task.add.html", $('div[data-tab="tasks"]'));
-    });
+$(document).on('click touchstart', function(e){
+    e.preventDefault();
+    console.log("clicked");
+    $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
+    $('#page1').trigger('pagecreate');
+    // $.mobile.loadPage("fragments/form.task.add.html", $('div[data-tab="tasks"]'));
 });
+
