88046d186ae8063c13304f58c278e3d71e88d0eb
Michael Müller <mishbag@reu-network.de>
2016-10-25 18:52:39 +0200

    bugfix: touch

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#btn-fab-add-task').bind('click touchstart', function(e){
+    $('#btn-fab-add-task').bind('click touch', function(e){
         e.preventDefault();
         $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
         $('#page1').trigger('pagecreate');
