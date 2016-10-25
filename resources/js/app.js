ff4db7296d3e675a7f5db31ec1d1b29236dde37a
Michael Müller <mishbag@reu-network.de>
2016-10-25 18:36:53 +0200

    bugfix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,7 @@
 
 $(document).ready(function(){
     $('#btn-fab-add-task').on('tap', function(){
-        $('div[data-tab="tasks"]').load("fragments/form.task.add.html");
-
-        $(document).trigger("createinclude");
+        $('#page1').trigger('pagecreate');
+        $.mobile.loadPage("fragments/form.task.add.html", $('div[data-tab="tasks"]'));
     });
 });
-
-function renderFragment(fragment)
-{
-    var el = this.element;
-    var opts = $.extend(this.options, el.data("options"));
-    $(document).trigger("createinclude");
-
-    if (opts.src !== null) {
-        el.load(opts.src, function() {
-            el.enhanceWithin();
-
-            // Apply waves.js
-            if (typeof Waves !== "undefined") {
-                Waves.attach('a', ['waves-button']);
-                Waves.attach('button', ['waves-button']);
-                Waves.init();
-
-                $("body").find(".ui-flipswitch-on").removeClass("waves-effect");
-                Waves.attach('.ui-flipswitch', ['waves-button', 'waves-light']);
-
-            }
-
-        });
-    }
-}
