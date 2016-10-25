07d1f19775e26f7d4cb1b920693e2cff80546db7
Michael Müller <mishbag@reu-network.de>
2016-10-25 19:08:20 +0200

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $('#btn-fab-add-task').on('vclick', function(e, ui){
-        e.preventDefault();
-        $(this).text("haha");
-        $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
-        $('#page1').trigger('pagecreate');
+
     });
 });
+
+function fabOnClickListener()
+{
+    $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
+    $('#page1').trigger('pagecreate');
+}
