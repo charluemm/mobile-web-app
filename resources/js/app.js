d85f4b9faef228130a3c86326cc704b2e18331ca
Michael Müller <mishbag@reu-network.de>
2016-10-25 18:41:50 +0200

    bugfix: load layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 
 $(document).ready(function(){
-    $('#btn-fab-add-task').on('tap', function(){
+    $('#btn-fab-add-task').click(function(e){
+        e.preventDefault();
+        console.log("clicked");
+        $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
         $('#page1').trigger('pagecreate');
-        $.mobile.loadPage("fragments/form.task.add.html", $('div[data-tab="tasks"]'));
+       // $.mobile.loadPage("fragments/form.task.add.html", $('div[data-tab="tasks"]'));
     });
 });
