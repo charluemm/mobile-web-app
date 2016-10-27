0110f2280a188741af73943226f7f66f2705b96a
Michael Müller <mishbag@reu-network.de>
2016-10-27 17:30:28 +0200

    removed add task form render

--- resources/js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#btn-fab-add-task').on('click', function(){
-        $('div[data-tab="tasks"]').html('<nd2-include data-src="fragments/form.task.add.html"></nd2-include>');
-        $('#page1').trigger('pagecreate');
-    });
-});
