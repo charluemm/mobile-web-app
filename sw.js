b7d52c481a7980590d2de4bb1f74098fb3675d51
Michael Müller <mishbag@reu-network.de>
2016-11-02 08:55:19 +0100

    moved material fonts and style

--- sw.js
+++ sw.js
@@ -3,9 +3,89 @@
  *
  * @author Michael Mueller <development@reu-network.de>
  */
-var VERSION = 'v11';
+var VERSION = 'v16';
 
 this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(VERSION).then(function(cache) {
+            return cache.addAll([
+                '/task-notification/',
+                '/task-notification/vendor/',
+
+                '/task-notification/vendor/nativedroid2/',
+                '/task-notification/vendor/nativedroid2/css/',
+                '/task-notification/vendor/nativedroid2/css/nativedroid2.css',
+                '/task-notification/vendor/nativedroid2/css/nativedroid2.color.blue-grey.css',
+                '/task-notification/vendor/nativedroid2/css/nativedroid2.color.teal.css',
+                '/task-notification/vendor/nativedroid2/css/flexboxgrid.min.css',
+                '/task-notification/vendor/nativedroid2/css/material-design-iconic-font.min.css',
+                '/task-notification/vendor/nativedroid2/fonts/',
+                '/task-notification/vendor/nativedroid2/fonts/Material-Design-Iconic-Font.eot',
+                '/task-notification/vendor/nativedroid2/fonts/Material-Design-Iconic-Font.svg',
+                '/task-notification/vendor/nativedroid2/fonts/Material-Design-Iconic-Font.ttf?v=2.0.0',
+                '/task-notification/vendor/nativedroid2/fonts/Material-Design-Iconic-Font.woff?v=2.0.0',
+
+                '/task-notification/vendor/nativedroid2/js/',
+                '/task-notification/vendor/nativedroid2/js/nativedroid2.js',
+
+                '/task-notification/vendor/font-awesome/',
+                '/task-notification/vendor/font-awesome/css/',
+                '/task-notification/vendor/font-awesome/css/font-awesome.min.css',
+                '/task-notification/vendor/font-awesome/fonts/',
+                '/task-notification/vendor/font-awesome/fonts/FontAwesome.otf',
+                '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.eot',
+                '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.svg',
+                '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.ttf?v=4.6.3',
+                '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.woff?v=4.6.3',
+                '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.woff2?v=4.6.3',
+
+                '/task-notification/vendor/jquery/',
+                '/task-notification/vendor/jquery/jquery-3.1.1.min.js',
+
+                '/task-notification/vendor/jquery/jquery-migrate-3.0.0.js',
+                '/task-notification/vendor/jquery-mobile/',
+                '/task-notification/vendor/jquery-mobile/jquery.mobile-1.4.5.min.js',
+                '/task-notification/vendor/jquery-mobile/jquery.mobile-1.4.5.min.css',
+                '/task-notification/vendor/jquery-mobile/images/',
+
+                '/task-notification/vendor/jquery-mobile/images/ajax-loader.gif',
+                '/task-notification/vendor/jquery-ui/',
+                '/task-notification/vendor/jquery-ui/images/',
+                '/task-notification/vendor/jquery-ui/jquery-ui.min.js',
+
+                '/task-notification/vendor/jquery-ui/jquery-ui.min.css',
+                '/task-notification/vendor/waves/',
+                '/task-notification/vendor/waves/waves.min.js',
+                '/task-notification/vendor/waves/waves.min.js.map',
+
+                '/task-notification/vendor/waves/waves.min.css',
+                '/task-notification/vendor/wow/',
+                '/task-notification/vendor/wow/animate.css',
+
+                '/task-notification/vendor/wow/wow.min.js',
+                '/task-notification/config/',
+                '/task-notification/config/nd2settings.js',
+                '/task-notification/fragments/',
+                '/task-notification/fragments/bottom.sheet.html',
+                '/task-notification/fragments/panel.left.html',
+                '/task-notification/resources/',
+                '/task-notification/resources/css/',
+                '/task-notification/resources/css/style.css',
+                '/task-notification/resources/img/',
+                '/task-notification/resources/img/examples/',
+                '/task-notification/resources/img/examples/card_bg_1.jpg',
+                '/task-notification/resources/img/examples/card_bg_2.jpg',
+                '/task-notification/resources/img/examples/card_bg_3.jpg',
+                '/task-notification/resources/img/examples/card_thumb_1.jpg',
+                '/task-notification/resources/img/examples/card_thumb_2.jpg',
+
+                '/task-notification/resources/img/examples/card_thumb_3.jpg',
+                '/task-notification/resources/js/',
+                '/task-notification/resources/js/app.js',
+                '/task-notification/index.html',
+            ]);
+        })
+    );
 });
 
 this.addEventListener('fetch', function(event) {
@@ -37,80 +117,3 @@ this.addEventListener('activate', function(event) {
         })
     );
 });
-
-event.waitUntil(
-    caches.open(VERSION).then(function(cache) {
-        return cache.addAll([
-            '/task-notification/',
-            '/task-notification/vendor/',
-
-            '/task-notification/vendor/nativedroid2/',
-            '/task-notification/vendor/nativedroid2/css/',
-            '/task-notification/vendor/nativedroid2/css/nativedroid2.css',
-            '/task-notification/vendor/nativedroid2/css/nativedroid2.color.blue-grey.css',
-            '/task-notification/vendor/nativedroid2/css/nativedroid2.color.teal.css',
-            '/task-notification/vendor/nativedroid2/css/flexboxgrid.min.css',
-            '/task-notification/vendor/nativedroid2/css/material-design-iconic-font.min.css',
-
-            '/task-notification/vendor/nativedroid2/js/',
-            '/task-notification/vendor/js/nativedroid2.js',
-
-            '/task-notification/vendor/font-awesome/',
-            '/task-notification/vendor/font-awesome/css/',
-            '/task-notification/vendor/font-awesome/css/font-awesome.min.css',
-            '/task-notification/vendor/font-awesome/fonts/',
-            '/task-notification/vendor/font-awesome/fonts/FontAwesome.otf',
-            '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.eot',
-            '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.svg',
-            '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.ttf',
-            '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.woff',
-
-            '/task-notification/vendor/font-awesome/fonts/fontawesome-webfont.woff2',
-            '/task-notification/vendor/jquery/',
-            '/task-notification/vendor/jquery/jquery-3.1.1.min.js',
-
-            '/task-notification/vendor/jquery/jquery-migrate-3.0.0.js',
-            '/task-notification/vendor/jquery-mobile/',
-            '/task-notification/vendor/jquery-mobile/jquery.mobile-1.4.5.min.js',
-            '/task-notification/vendor/jquery-mobile/jquery.mobile-1.4.5.min.css',
-            '/task-notification/vendor/jquery-mobile/images/',
-
-            '/task-notification/vendor/jquery-mobile/images/ajax-loader.gif',
-            '/task-notification/vendor/jquery-ui/',
-            '/task-notification/vendor/jquery-ui/images/',
-            '/task-notification/vendor/jquery-ui/jquery-ui.min.js',
-
-            '/task-notification/vendor/jquery-ui/jquery-ui.min.css',
-            '/task-notification/vendor/waves/',
-            '/task-notification/vendor/waves/waves.min.js',
-            '/task-notification/vendor/waves/waves.min.js.map',
-
-            '/task-notification/vendor/waves/waves.min.css',
-            '/task-notification/vendor/wow/',
-            '/task-notification/vendor/wow/animate.css',
-
-            '/task-notification/vendor/wow/wow.min.js',
-            '/task-notification/config/',
-            '/task-notification/config/nd2settings.js',
-            '/task-notification/fragments/',
-            '/task-notification/fragments/bottom.sheet.html',
-            '/task-notification/fragments/panel.left.html',
-            '/task-notification/resources/',
-            '/task-notification/resources/css/',
-            '/task-notification/resources/css/style.css',
-            '/task-notification/resources/fonts/',
-            '/task-notification/resources/img/',
-            '/task-notification/resources/img/examples/',
-            '/task-notification/resources/img/examples/card_bg_1.jpg',
-            '/task-notification/resources/img/examples/card_bg_2.jpg',
-            '/task-notification/resources/img/examples/card_bg_3.jpg',
-            '/task-notification/resources/img/examples/card_thumb_1.jpg',
-            '/task-notification/resources/img/examples/card_thumb_2.jpg',
-
-            '/task-notification/resources/img/examples/card_thumb_3.jpg',
-            '/task-notification/resources/js/',
-            '/task-notification/resources/js/app.js',
-            '/task-notification/index.html',
-        ]);
-    })
-);
