33442dbbbf73518efae6357acd9dcca53efcd8a3
Michael Müller <mishbag@reu-network.de>
2016-11-07 20:39:20 +0100

    added login api call

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,10 @@
  * @author Michael Mueller <development@reu-network.de>
  * @author David Howon <maestroderjoker@googlemail.com>
  */
+
 if ('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+    navigator.serviceWorker.register('./sw.js', { scope: "./" }).then(function(reg) {
 
         if(reg.installing)
         {
@@ -25,3 +26,31 @@ if ('serviceWorker' in navigator)
         console.log('Registration failed with ' + error);
     });
 }
+
+
+// LOGIN
+$('#submit-login').on('click', function(e){
+    e.preventDefault();
+    var data = $('#frmLogin').serialize();;
+    $.ajax({
+        type: "POST",
+        // TODO: load URL from config
+        url: "http://localhost/nodejs/api/authenticate",
+        data: data,
+        success: function(data){
+            if(data.success)
+                // TODO: save token in local storage
+                jQuery.mobile.changePage("#Home");
+            else
+                new $.nd2Toast({
+                    message: data.message,
+                    ttl: 3000
+                });
+
+            console.log(data);
+        },
+        error: function(xhr, options, error){
+            console.log("Error");
+        }
+    });
+});
