1752b05136efb734758cf7497b49df5e065a0782
Michael Müller <mishbag@reu-network.de>
2016-11-08 13:15:58 +0100

    remvoed scope option

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 
 if ('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('./sw.js', { scope: "./" }).then(function(reg) {
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
 
         if(reg.installing)
         {
