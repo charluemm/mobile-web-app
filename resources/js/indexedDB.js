5c737e63ee05dd2abe9b2299cea8b32fd15efe3b
Michael Müller <mishbag@reu-network.de>
2017-02-12 20:13:36 +0100

    added some stuff

---
+++ resources/js/indexedDB.js
@@ -0,0 +1,31 @@
+/**
+ * file to configure and handle indexDB
+ *
+ * @author Michael Mueller <development@reu-network.de>
+ */
+
+// configure indexDB
+// window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+// window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+function updateTasks(collection)
+{
+       var db = idb.open('TaskyAppState', 1, function(upgradeDb) {
+           const tasks = upgradeDb.createObjectStore('tasks', {
+               keyPath: '_id'
+           });
+
+           // 2. Open a new read/write transaction with the store within the database
+           const transaction = db.transaction('tasks', 'readwrite');
+           const store = transaction.objectStore('tasks');
+
+           // 3. Add the data to the store
+           collection.forEach(function (entry) {
+               console.log('item:', entry);
+               store.put(entry);
+           });
+
+           // 4. Complete the transaction
+           return transaction.complete;
+       })
+}
